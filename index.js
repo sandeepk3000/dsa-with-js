afff374a9e782213ff09176af7fce06988fd4d60
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-05 17:58:38 +0000

    second array operation
    2 time i write some  array operation code

--- index.js
+++ index.js
@@ -66,3 +66,101 @@ for (let i = 0; i < arr.length; i++) {
   }
 }
 console.log(max, min);
+
+// Count pairs in an array whose sum is equal to x
+
+let arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let x6 = 7;
+let count6= 0;
+for(let i = 0; i < arr6.length; i++){
+  for(let j = i+1; j < arr6.length; j++){
+    if(arr6[i]+arr6[j] == x6){
+      count6++;
+    }
+  }
+}
+console.log(count6);
+
+// Remove duplicates from an array
+let arr7 = [1,2,3,1,2,5,4,3]
+
+for(let i = 0; i < arr7.length; i++){
+  for(let j = i+1; j < arr7.length; j++){
+    if(arr7[i] == arr7[j]){
+      arr7[j] = -1;
+      arr7[i] = -1;
+    }
+  }
+}
+console.log(arr7)
+
+// Find the first and second largest number in an array
+let arr8 = [8, 1, 2, 3, 4, 10, 5, 6, 7, 9];
+let firstLarget = -Infinity;
+let secondLarget = -Infinity;
+for (let i = 0; i < arr8.length; i++) {
+  if (arr8[i] > firstLarget) {
+    secondLarget = firstLarget;
+    firstLarget = arr8[i];
+  }
+}
+console.log(firstLarget, secondLarget);
+
+// Swap two numbers
+function swapNumber(a, b) {
+  let temp = a;
+  a = b;
+  b = temp;
+  console.log("the value of a is ", a);
+  console.log("the value of b is ", b);
+}
+swapNumber(10,4)
+
+
+// Swap two numbers without using a third variable
+function swapNumberUsingDifference(a, b) {
+  a = a + b
+  b = a - b
+  a = a - b
+  console.log("the value of a is ", a);
+  console.log("the value of b is ", b);
+}
+swapNumberUsingDifference(10,4)
+
+// Rotate an array by k elements
+let arr9 = [1,2,3,4,5,6,7,8,9]
+let k1 = 3
+k1 = k1 % arr9.length
+let ans7 = new Array(arr9.length)
+let j = 0
+for(let i = arr9.length - k ; i < arr9.length;i++){
+  ans7[j++] = arr9[i]
+}
+for(let i = 0; i < arr9.length - k1 ;i++){
+  ans7[j++] = arr9[i]
+}
+console.log(ans7)
+
+// Rotate an array by k elements without using extra space
+function swapArrayNumber(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function reverseArray(arr, start, end) {
+  while (start < end) {
+    swapArrayNumber(arr, start, end);
+    start++;
+    end--;
+  }
+}
+
+let arr10 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let n = arr10.length;
+let k2 = 10;
+k2 = k2 % n;
+reverseArray(arr10, 0, (n - k2) - 1);
+reverseArray(arr10, n - k2, n - 1);
+reverseArray(arr10, 0, n - 1);
+console.log(arr10);
