7459c67525cda0ed701677557f6c6b3b50e9d6e4
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-07 12:00:12 +0000

    export all func

--- src/array/prefixSum.js
+++ src/array/prefixSum.js
@@ -18,3 +18,5 @@ function prefixSumWithOutExtraSpace(arr) {
   return arr;
 }
 console.log(prefixSumWithOutExtraSpace([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+export { prefixSumWithExtraSpace, prefixSumWithOutExtraSpace };
