2320f9e363cc30356af9de1f6b187f382bc505ad
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-07 11:11:34 +0000

    in array complete prefix sum

---
+++ src/array/prefixSum.js
@@ -0,0 +1,20 @@
+// Prefix Sum with extra space
+
+function prefixSumWithExtraSpace(arr) {
+  let prefix = new Array(arr.length);
+  prefix[0] = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    prefix[i] = prefix[i - 1] + arr[i];
+  }
+  return prefix;
+}
+console.log(prefixSumWithExtraSpace([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// Prefix Sum without extra space
+function prefixSumWithOutExtraSpace(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i - 1] + arr[i];
+  }
+  return arr;
+}
+console.log(prefixSumWithOutExtraSpace([1, 2, 3, 4, 5, 6, 7, 8, 9]));
