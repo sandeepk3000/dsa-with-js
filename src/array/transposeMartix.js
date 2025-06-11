cc3ea422cbfd160128425211f565e1ead69887f2
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-11 18:09:10 +0000

    add 4  func transpose,transposeInPlace,rotate,print matrix

--- src/array/transposeMartix.js
+++ src/array/transposeMartix.js
@@ -1,4 +1,7 @@
 import { printArray } from "./2DArray.js";
+import { reverseArray } from "./reverseArray.js";
+
+// Transpose of a matrix
 function transposeMatrix(matrix, row, col) {
   let transpose = Array.from({ length: row }, () =>
     Array.from({ length: col }, () => 0),
@@ -21,3 +24,41 @@ transposeMatrix(
   3,
   3,
 );
+
+// Transpose of a matrix in place
+function transposeInPlace(matrix, row, col) {
+  for (let i = 0; i < row; i++) {
+    for (let j = i; j < col; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  printArray(matrix);
+}
+transposeInPlace(
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  3,
+  3,
+);
+
+// Rotate a matrix by 90 degrees
+function rotate(matrix, n) {
+  transposeInPlace(matrix, n, n);
+  for (let i = 0; i < n; i++) {
+    reverseArray(matrix[i]);
+  }
+  printArray(matrix);
+}
+rotate(
+  [
+    [1, 2],
+    [4, 5],
+  ],
+  2,
+);
+export { transposeMatrix, transposeInPlace, rotate, printArray };
