48f46e2e8f86c97b7dafc1e9d6ec4eb26ce3559e
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-16 19:08:57 +0000

    add rectangleSumM1 , rectangleSumM2  func

---
+++ src/array/rectangleSum.js
@@ -0,0 +1,58 @@
+let arr = [
+  [1, 2, 2, 2, 3],
+  [1, 2, 2, 2, 3],
+  [1, 2, 2, 2, 3],
+  [1, 2, 2, 2, 3],
+];
+
+function rectangleSumM1(arr, l1, r1, l2, r2) {
+  let sum = 0;
+  for (let i = l1; i <= l2; i++) {
+    for (let j = r1; j <= r2; j++) {
+      sum += arr[i][j];
+    }
+  }
+  console.log(sum);
+}
+function prefixSum(arr, n, m) {
+  for (let i = 0; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      arr[i][j] += arr[i][j - 1];
+    }
+  }
+}
+// function rectangleSumM2(arr, l1, r1, l2, r2) {
+//   let sum = 0;
+//   prefixSum(arr, l2, r2);
+//   console.log(arr);
+//   for (let i = l1; i <= l2; i++) {
+//     sum += arr[i][r2] - arr[i][r1 - 1];
+//   }
+//   console.log(sum);
+// }
+// function prefixSum2D(arr, n, m) {
+//   for (let i = 0; i <= n; i++) {
+//     for (let j = 1; j <= m; j++) {
+//       arr[i][j] += arr[i][j - 1];
+//     }
+//   }
+//   for (let j = 0; j <= m; j++) {
+//     for (let i = 1; i <= n; i++) {
+//       arr[i][j] += arr[i - 1][j];
+//     }
+//   }
+//   console.log(arr);
+// }
+// function rectangleSumM3(arr, l1, r1, l2, r2) {
+//   prefixSum2D(arr, l2, r2);
+//   console.log(arr);
+//   let sum = arr[l2][r2];
+//   let left = arr[l2][r1 - 1];
+//   let up = arr[l1 - 1][r2];
+//   let leftup = arr[l1 - 1][r1 - 1];
+//   let ans = sum - left - up - leftup + up;
+//   console.log(ans);
+// }
+rectangleSumM1(arr, 1, 2, 3, 4);
+rectangleSumM2(arr, 1, 2, 3, 4);
+rectangleSumM3(arr, 1, 2, 3, 4);
