0c0fcd5ab1f3b495ece2f0a7d2ef0a86b63a7a2a
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-18 17:46:15 +0000

    add printDecreasing func using recursion

--- src/recursion/printNaturalNo.js
+++ src/recursion/printNaturalNo.js
@@ -1,10 +1,17 @@
-
-function printNaturalNo(n){
-  if(n===1){//base case
-    console.log(1)
+function printNaturalNo(n) {
+  if (n === 1) {
+    //base case
+    console.log(1);
     return;
   }
-  printNaturalNo(n-1) //assume
-  console.log(n)//self work
+  printNaturalNo(n - 1); //assume
+  console.log(n); //self work
 }
-printNaturalNo(5)
+printNaturalNo(5);
+
+function printDecreasing(n){
+  if(n === 0)return;
+  console.log(n)
+  printDecreasing(n-1)  
+}
+// printDecreasing(4)
